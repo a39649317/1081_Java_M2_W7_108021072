a5c06c3088d64bcfe0f3539a3a07fdcbf30f5d0f
a39649317 <a0978918687>
2019-10-23 23:01:30 +0800

    M2Q10

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int sum = 0 ;
-        for (int i = 1 ; i<=n ; i++ ){
-            sum = sum +i*(i+1);
-        }
-        System.out.println(sum);
+        for (int i = 1 ; i<=n ; i++){
+            for(int j =1; j<=n ; j++){
+                System.out.print((i*j)+"\t");
+            }
+            System.out.println();
+            }
     }
 }
