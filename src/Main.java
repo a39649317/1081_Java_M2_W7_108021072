0a8096c4091d4250d66f2be188fc2d51264de7f3
a39649317 <a0978918687>
2019-10-23 21:32:58 +0800

    M2Q8

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int m = n/2;
-        boolean flag = true;
-        for(int i =2 ; i <=m ; i++){
-            if(n%i==0){
-                flag = false;
-                break;
-            }
+        int sum = 0 ;
+        for (int i = 3 ; i<=n ; i+=3 ){
+            sum = sum +i;
         }
-        if(flag){
-            System.out.println("YES");
-        }
-        else{
-            System.out.println("NO");
-        }
-
+        System.out.println(sum);
     }
 }
