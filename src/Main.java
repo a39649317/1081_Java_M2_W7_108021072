26956567ecfc4195e44dde6965907fd630d83cb2
a39649317 <a0978918687>
2019-10-23 21:37:00 +0800

    M2Q14

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int sum = 0 ;
-        for (int i = 3 ; i<=n ; i+=3 ){
-            sum = sum +i;
+        for (int i = 1 ; i<=n ; i++ ){
+            sum = sum +i*(i+1);
         }
         System.out.println(sum);
     }
