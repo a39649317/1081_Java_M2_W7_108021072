3ff22c842f59586a39afc12df884560e251e62eb
a39649317 <a0978918687>
2019-10-27 14:28:47 +0800

    M2Q13

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        long sum = 1;
         for (int i = 1 ; i<=n ; i++){
-            for(int j =1; j<=n ; j++){
-                System.out.print((i*j)+"\t");
-            }
-            System.out.println();
-            }
+            sum *= i;
+        }
+        System.out.println(sum);
     }
 }
